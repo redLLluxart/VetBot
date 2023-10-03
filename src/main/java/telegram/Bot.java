1ded58df50d9e9b1a6969f75d01b6a03e75da4ab
Pavel Kulyamin <kulyamin.pavel@yandex.ru>
2023-10-03 21:23:39 +0300

    Add some fix

--- src/main/java/telegram/Bot.java
+++ src/main/java/telegram/Bot.java
@@ -8,38 +8,50 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import shedule.SheduleFunctions;
+
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
+
 public class Bot extends TelegramLongPollingBot {
 
-    Properties property = new Properties();
-    private final String BotToken=property.getProperty("token");
-    private final String BotName=property.getProperty("name");
     String group = null;
+
+    private Properties getPropertyFromFile() {
+
+        String rootPath = "G:\\Red\\VetBot\\src\\main\\resources\\config.properties";
+        Properties props = new Properties();
+        try {
+            props.load(new FileInputStream(rootPath));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return props;
+    }
 
 
     @Override
     public String getBotUsername() {
-        return BotName;
+        return getPropertyFromFile().getProperty("name");
     }
 
     @Override
     public String getBotToken() {
-        return BotToken;
+        return getPropertyFromFile().getProperty("token");
     }
 
     @SneakyThrows
     @Override
     public void onUpdateReceived(Update update) {
-        if(update.hasMessage() && update.getMessage().getText().equals("/start")){ // check hasMessage first! if not check hasMessage(), it call getMessage(), it breaks into runtimeException
+        if (update.hasMessage() && update.getMessage().getText().equals("/start")) { // check hasMessage first! if not check hasMessage(), it call getMessage(), it breaks into runtimeException
             String chatId = String.valueOf(update.getMessage().getChatId());
             sendMessage("Все пары проходят в промежутке 18:00 - 21:15.\n(Гл) Главный корпус\n(Им) Здание каф. Иммунологии\n" +
                     "(Ак) Здание каф. Акушерства\n(Улк) Учебно-Лабораторный корпус\n" +
-                    "(Ан) Анатомический корпус\n(Дос) Дос-2; (Лб) Лаборатория биофизики\n",chatId);
-            sendMarkup(CreateMenu.createMenuMarkupGroup(), chatId,"Привет!\n" +
+                    "(Ан) Анатомический корпус\n(Дос) Дос-2; (Лб) Лаборатория биофизики\n", chatId);
+            sendMarkup(CreateMenu.createMenuMarkupGroup(), chatId, "Привет!\n" +
                     "Чтобы получить расписание выбери группу в которой ты учишься.");
-        } else if(update.hasCallbackQuery()){
+        } else if (update.hasCallbackQuery()) {
             handleCallback(update);
         }
     }
@@ -55,7 +67,7 @@ public class Bot extends TelegramLongPollingBot {
                 sendMessage("Вы выбрали группу 1.", chatId);
                 group = "1";
                 execute(close);
-                sendMarkup(CreateMenu.createMenuMarkupGroup(), chatId,"Выбери период времени для которого тебе требуется расписание.");
+                sendMarkup(CreateMenu.createMenuMarkupGroup(), chatId, "Выбери период времени для которого тебе требуется расписание.");
                 break;
             case "2":
                 sendMessage("Вы выбрали группу 2.", chatId);
@@ -76,15 +88,15 @@ public class Bot extends TelegramLongPollingBot {
                 sendMarkup(CreateMenu.createMenuMarkupPeriod(), chatId, "Выбери период времени для которого тебе требуется расписание.");
                 break;
             case "Today":
-                sendMessage("Сегодня у вас будет:\n"+ SheduleFunctions.getTodayShedule(group),chatId);
+                sendMessage("Сегодня у вас будет:\n" + SheduleFunctions.getTodayShedule(group), chatId);
                 execute(close);
                 break;
             case "Next lesson":
-                sendMessage("Ваше следующее занятие:\n"+SheduleFunctions.getNextShedule(group),chatId);
+                sendMessage("Ваше следующее занятие:\n" + SheduleFunctions.getNextShedule(group), chatId);
                 execute(close);
                 break;
             case "This week":
-                sendMessage("Ваше расписание на эту неделю:\n"+SheduleFunctions.getTodayWeekShedule(group).toString(),chatId);
+                sendMessage("Ваше расписание на эту неделю:\n" + SheduleFunctions.getTodayWeekShedule(group).toString(), chatId);
                 execute(close);
                 break;
             default:
